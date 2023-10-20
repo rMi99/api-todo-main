3539e8dc7dec04a5c0c8a3c7664734bce7495d27
rMi99 <99ramigimhama@gmail.com>
2023-10-20 08:26:37 +0530

    Add Google Auth

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,13 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
-    protected $fillable = [
-        'id',
-        'task',
-        'description',
-        'is_completed',
-        'link',
-        'user_id',
-        'due_date',
-    ];
+    protected $guarded= [];
+    // protected $fillable = [
+    //     'id',
+    //     'task',
+    //     'description',
+    //     'is_completed',
+    //     'link',
+    //     'user_id',
+    //     'due_date',
+    // ];
 }
